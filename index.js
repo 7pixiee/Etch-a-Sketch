72b334fd490cfb2c32bb71a45698e4f12ec96a18
Tulip <sumayyabelal7@gmail.com>
2026-07-26 18:46:32 +0000

    Initial project setup with dynamic grid

---
+++ index.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector(".canvas");
+const sizeButton = document.querySelector(".grid-size");
+const clearButton = document.querySelector(".grid-clear");
+
+function createCells(size){
+    canvas.innerHTML = "";
+
+    for(let i = 0; i < size * size; i++){
+        const cell = document.createElement("div");
+        cell.classList.add('grid-cell');
+          const cellSize = 500 / size;
+cell.style.width = `${cellSize}px`;
+cell.style.height = `${cellSize}px`;
+        canvas.appendChild(cell);
+    }
+};
+
+sizeButton.addEventListener("click", () => {
+    const size = prompt("Enter size between 1 to 100");
+    createCells(size);
+});
