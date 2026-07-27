d59c0b8689e797d5b92af434d58a560d7fae3bfd
Tulip <sumayyabelal7@gmail.com>
2026-07-27 09:40:12 +0000

    Implement drawing and opacity shading

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const canvas = document.querySelector(".canvas");
 const sizeButton = document.querySelector(".grid-size");
 const clearButton = document.querySelector(".grid-clear");
 
-function createCells(size){
+function createCells(size) {
     canvas.innerHTML = "";
 
-    for(let i = 0; i < size * size; i++){
+    for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
         cell.classList.add('grid-cell');
-          const cellSize = 500 / size;
-cell.style.width = `${cellSize}px`;
-cell.style.height = `${cellSize}px`;
+        const cellSize = 500 / size;
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
+        cell.dataset.opacity = "0";
         canvas.appendChild(cell);
-        cell.addEventListener("mousemove", () => {
-            cell.style.backgroundColor = "pink";
-        })
-    }
+
+        cell.addEventListener("mouseover", () => {
+            let opacity = Number(cell.dataset.opacity);
+            opacity += 0.1;
+
+            if (opacity > 1) {
+                opacity = 1;
+            }
+
+            cell.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+            cell.dataset.opacity = opacity;
+        });
+    };
 };
 
 sizeButton.addEventListener("click", (e) => {
